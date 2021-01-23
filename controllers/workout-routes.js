be497b5ed9a4ab360e080c2a5748589f46ce314d
Brenda Goodwin <brendanced@gmail.com>
2021-01-23 13:29:52 -0700

    front end connected to backend

--- controllers/workout-routes.js
+++ controllers/workout-routes.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
-const Workout = require("../models/workout");
+const Workout = require("../models/Workout.js");
+const { db } = require("../models/Workout.js");
 
 //get access to workouts in date order
 router.get("/api/workout", (req, res) => {
   Workout.find()
-    .sort({ date: -1 })
     .then((dbData) => {
       res.json(dbData);
       console.log(dbData);
@@ -38,6 +38,26 @@ router.post("/api/workout", (req, res) => {
   Workout.create({})
     .then((dbData) => {
       res.jason(dbData);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+//Aggregate weight and duration
+router.get("/api/workout", (req, res) => {
+  Workout.find()
+    .sort({ date: -1 })
+    .limit(7)
+    .then((dbData) => {
+      db.workout.aggregate([
+        {
+          $addFields: {
+            totalWeight: { $sum: "$weight" },
+            totalDuration: { $sum: "$duration" },
+          },
+        },
+      ]);
     })
     .catch((err) => {
       res.status(400).json(err);
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,9 +16,15 @@ mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/workoutTracker",
   {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+    useCreateIndex: true,
     useFindAndModify: false,
   }
 );
+
+app.use(require("./controllers/home-routes.js"));
+app.use(require("./controllers/workout-routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App is now running on ${PORT}, Thank Goodness.`);
